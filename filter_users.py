bd6b3d0f6e2262dd4b9027537cc3d0605872a064
Ivan Gaskell <ivangaskell@gmail.com>
2026-09-15 17:43:39 +0200

    Added by email function

--- filter_users.py
+++ filter_users.py
@@ -1,5 +1,14 @@
 import json
 
+def filter_users_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user.get("email", "").lower() == email.lower()]
+
+    for user in filtered_users:
+        print(user)
+
 def filter_users_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)

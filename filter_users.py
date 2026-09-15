736ab41f89a1728b86aa4797e369a2032c86fa76
Ivan Gaskell <ivangaskell@gmail.com>
2026-09-15 17:35:30 +0200

    Added by age function

--- filter_users.py
+++ filter_users.py
@@ -1,5 +1,14 @@
 import json
 
+def filter_users_by_age(age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+  
+    filtered_users = [user for user in users if user.get("age") == age]
+  
+    for user in filtered_users:
+        print(user)
+
 def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
